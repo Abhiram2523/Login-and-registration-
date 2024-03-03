6710f043aeb73eb5371676e9a04f70220f259119
Abhiram2523 <abhiram2523@gmail.com>
2024-03-04 02:52:22 +0530

    next update

--- src/main/java/com/company/util/DBUtil.java
+++ src/main/java/com/company/util/DBUtil.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-	 private static final String URL = "jdbc:mysql://localhost:3306/codingwallah";
+	 private static final String URL = "jdbc:mysql://localhost:3306/loginandregistration";
 	 private static final String USERNAME = "root";
-	 private static final String PASSWORD = "password";
+	 private static final String PASSWORD = "abhi";
     static {
         try {
             // Load the MySQL JDBC driver
